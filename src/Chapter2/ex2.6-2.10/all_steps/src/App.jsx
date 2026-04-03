6832387ea0e64cfe42265297786656e51f285e77
Junaid Iqbal <junaidiqbal0022@yahoo.com>
2026-04-03 22:26:28 +0300

    ex2.6: The Phonebook Step 1

---
+++ src/Chapter2/ex2.6-2.10/all_steps/src/App.jsx
@@ -0,0 +1,50 @@
+import { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas", id: 1 }]);
+  const [newName, setNewName] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    const personObject = {
+      name: newName,
+      id: persons.length + 1,
+    };
+    console.log("personObject is", personObject);
+    setPersons(persons.concat(personObject));
+    setNewName("");
+  };
+  const onChangeName = (event) => {
+    console.log("name is", event.target.value);
+    setNewName(event.target.value);
+  };
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={onSubmit}>
+        <div>
+          name:
+          <input
+            id="name_field"
+            value={newName}
+            onChange={(e) => onChangeName(e)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {/* <div>debug: {newName}</div> */}
+      {persons.map((person) => (
+        //can there be duplicated names?
+        // if not, we can use name as key,
+        // otherwise we need to generate a unique id for each person
+        //<div key={person.name}>{person.name}</div>
+        <div key={person.id}>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
