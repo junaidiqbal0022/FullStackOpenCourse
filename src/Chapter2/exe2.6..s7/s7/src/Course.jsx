6d980958ae1d8c1608daa28dd23605a3d9c3ef36
Junaid Iqbal <junaidiqbal0022@yahoo.com>
2026-04-03 17:44:04 +0300

    ex2.2

--- src/Chapter2/exe2.6..s7/s7/src/Course.jsx
+++ src/Chapter2/exe2.6..s7/s7/src/Course.jsx
@@ -1,10 +1,19 @@
 import { Header } from "./components/Header";
 import Content from "./components/Content";
+import Total from "./components/Total";
 const Course = ({ course }) => {
+  const handleTotal = () => {
+    var sum = 0;
+    for (var i = 0; i < course.parts.length; i++) {
+      sum += course.parts[i].exercises;
+    }
+    return sum;
+  };
   return (
     <div>
       <Header course={course.name} />
       <Content parts={course.parts} />
+      <Total total={handleTotal()} />
     </div>
   );
 };
---
+++ src/Chapter2/exe2.6..s7/s7/src/components/Total.jsx
@@ -0,0 +1,9 @@
+const Total = ({ total }) => {
+  return (
+    <>
+      <h2>total of {total} exercises</h2>
+    </>
+  );
+};
+
+export default Total;
