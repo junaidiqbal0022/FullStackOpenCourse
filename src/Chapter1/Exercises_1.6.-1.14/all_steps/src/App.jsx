be6e24f37cd257d032b861a2ccd033d29983355e
Junaid Iqbal <junaidiqbal0022@yahoo.com>
2026-04-03 11:17:18 +0300

    ex 1,13

--- src/Chapter1/Exercises_1.6.-1.14/all_steps/src/App.jsx
+++ src/Chapter1/Exercises_1.6.-1.14/all_steps/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { Button } from "./Button";
 import { Statistics } from "./Statistics";
+import { StatisticLine } from "./StatisticLine";
 const App = () => {
   const Feedback = {
     GOOD: "good",
@@ -27,15 +28,47 @@ const App = () => {
     setBad(tempSet);
     setAll(all.concat(Feedback.BAD));
   };
+  const anecdotes = [
+    "If it hurts, do it more often.",
+    "Adding manpower to a late software project makes it later!",
+    "The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.",
+    "Any fool can write code that a computer can understand. Good programmers write code that humans can understand.",
+    "Premature optimization is the root of all evil.",
+    "Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.",
+    "Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.",
+    "The only way to go fast, is to go well.",
+  ];
+  const [selected, setSelected] = useState(0);
+  const onNextAnecdote = () => {
+    const num = Math.floor(Math.random() * anecdotes.length);
+    setSelected(num);
+    console.log(`selected: ${num} anecdote: ${anecdotes[num]}`);
+  };
+  const [votes, setVotes] = useState(Array(anecdotes.length).fill(0));
 
+  const onVote = () => {
+    const copy = [...votes];
+    copy[selected] += 1;
+    setVotes(copy);
+    console.log(
+      `vote for: ${anecdotes[selected]}, Now it has ${copy[selected]} votes`,
+    );
+  };
   return (
-    <div>
-      <h1>give feedback</h1>
-      <Button onClick={() => onGood(good)} text="good" />
-      <Button onClick={() => onNeutral(neutral)} text="neutral" />
-      <Button onClick={() => onBad(bad)} text="bad" />
-      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
-    </div>
+    <>
+      {/* // <div>
+    //   <h1>give feedback</h1>
+    //   <button onclick={() => ongood(good)} text="good" />
+    //   <button onclick={() => onneutral(neutral)} text="neutral" />
+    //   <button onclick={() => onbad(bad)} text="bad" />
+    //   <statistics good={good} neutral={neutral} bad={bad} all={all} />
+    // </div> */}
+      <div>{anecdotes[selected]}</div>
+      <StatisticLine text="has votes" value={votes[selected]} />
+
+      <Button text="next anecdote" onClick={onNextAnecdote}></Button>
+      <Button text="vote" onClick={onVote}></Button>
+    </>
   );
 };
 
--- src/Chapter1/Exercises_1.6.-1.14/all_steps/src/Button.jsx
+++ src/Chapter1/Exercises_1.6.-1.14/all_steps/src/Button.jsx
@@ -4,8 +4,8 @@ export const Button = ({ onClick, text }) => {
   return (
     <button
       style={{
-        width: "100px",
-        height: "40px",
+        minWidth: "100px",
+        minHeight: "40px",
         margin: "5px",
         padding: "5px",
         fontSize: "16px",
