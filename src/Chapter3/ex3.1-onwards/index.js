706d8d978b8fd5e94344298b6b46725519104d99
Junaid Iqbal <junaidiqbal0022@yahoo.com>
2026-04-07 20:04:55 +0300

    Exercise 3.12.

--- src/Chapter3/ex3.1-onwards/index.js
+++ src/Chapter3/ex3.1-onwards/index.js
@@ -20,28 +20,6 @@ var logger = morgan(function (tokens, req, res) {
 })
 app.use(logger)
 
-let phoneBook = [
-    {
-        "id": "1",
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": "2",
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": "3",
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": "4",
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-];
 
 app.get('/', (request, response) => {
     response.send('<h1>Welcom to Phone Book! This is radio station Tampere and today is: </br>' + new Date() + '</h1>')
